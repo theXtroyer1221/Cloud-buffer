6ffba85a1078d088ddb2ba98c3e0d906d7bb57a4
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-05-03 22:00:20 +0200

    added the database and alos

---
+++ cloudBuffer/Lib/site-packages/google_images_search/google_api.py
@@ -0,0 +1,109 @@
+import os
+import requests
+from apiclient.discovery import build
+
+
+class GoogleCustomSearch(object):
+    """Wrapper class for Google images search api"""
+
+    def __init__(self, developer_key=None,
+                 custom_search_cx=None,
+                 fethch_resize_save=None):
+
+        self._developer_key = developer_key or \
+                              os.environ.get('GCS_DEVELOPER_KEY')
+        self._custom_search_cx = custom_search_cx or \
+                                 os.environ.get('GCS_CX')
+
+        self._google_build = None
+        self._fethch_resize_save = fethch_resize_save
+
+        self._search_params_keys = {
+            'q': None,
+            'searchType': 'image',
+            'num': 1,
+            'start': 1,
+            'imgType': None,
+            'imgSize': None,
+            'fileType': None,
+            'safe': 'off',
+            'imgDominantColor': None
+        }
+
+    def _query_google_api(self, search_params, cache_discovery=True):
+        """Queries Google api
+        :param search_params: dict of params
+        :param cache_discovery whether or not to cache the discovery doc
+        :return: search result object
+        """
+
+        if not self._google_build:
+            self._google_build = build("customsearch", "v1",
+                                       developerKey=self._developer_key,
+                                       cache_discovery=cache_discovery)
+
+        return self._google_build.cse().list(
+            cx=self._custom_search_cx, **search_params).execute()
+
+    def _search_params(self, params):
+        """Received a dict of params and merges
+        it with default params dict
+        :param params: dict
+        :return: dict
+        """
+
+        search_params = {}
+
+        for key, value in self._search_params_keys.items():
+            params_value = params.get(key)
+            if params_value:
+                # take user defined param value if defined
+                search_params[key] = params_value
+            elif value:
+                # take default param value if defined
+                search_params[key] = value
+
+        return search_params
+
+    def search(self, params, cache_discovery=True):
+        """Search for images and returns
+        them using generator object
+        :param params: search params
+        :param cache_discovery whether or not to cache the discovery doc
+        :return: yields url to searched image
+        """
+
+        search_params = self._search_params(params)
+
+        res = self._query_google_api(search_params, cache_discovery)
+
+        for image in res.get('items', []):
+            try:
+                response = requests.head(image['link'], timeout=5)
+                content_length = response.headers.get('Content-Length')
+
+                # check if the url is valid
+                if response.status_code == 200 and \
+                        'image' in response.headers['Content-Type'] and \
+                        content_length:
+
+                    # calculate download chunk size based on image size
+                    self._fethch_resize_save.set_chunk_size(
+                        image['link'], content_length
+                    )
+
+                    # if everything is ok, yield image url back
+                    yield image['link']
+
+                else:
+                    # validation failed, go with another image
+                    continue
+
+            except requests.exceptions.ConnectTimeout:
+                pass
+            except requests.exceptions.SSLError:
+                pass
+
+
+class GoogleBackendException(Exception):
+    """Exception handler for search api"""
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from forms import SearchForm, locationSearch, RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 import json
 import requests
+from datetime import datetime
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,6 +17,7 @@ from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Cloud Buffer"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 GCS_DEVELOPER_KEY = os.environ.get('GCS_DEVELOPER_KEY')
@@ -22,6 +25,35 @@ GCS_CX = os.environ.get('GCS_CX')
 IP_STACK = os.environ.get("IP_STACK")
 
 app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies=1)
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20),
+                           nullable=False,
+                           default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime,
+                            nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 @app.route("/", methods=["GET", "POST"])
