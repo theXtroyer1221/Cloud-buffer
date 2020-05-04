196da1cd66c6909a2ee07b2ffe5e69b98aff78b4
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-05-04 21:38:07 +0200

    added the picture option, validation not working

--- cloudBuffer/routes.py
+++ cloudBuffer/routes.py
@@ -1,4 +1,4 @@
-from cloudBuffer.forms import SearchForm, locationSearch, RegistrationForm, LoginForm
+from cloudBuffer.forms import SearchForm, locationSearch, RegistrationForm, LoginForm, UpdateAccountForm
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, current_user, logout_user, login_required
 from cloudBuffer.models import User, Post
@@ -6,6 +6,7 @@ from cloudBuffer import app, bcrypt, db, login_manager
 
 import requests
 import os
+import secrets
 
 from google_images_search import GoogleImagesSearch
 
@@ -112,7 +113,13 @@ def search(query):
 
 @app.route("/blog")
 def blog():
-    return render_template("blog.html", data="data", title="Blog")
+    if current_user.is_authenticated:
+        image_file = url_for("static",
+                            filename="profile_pics/" + current_user.image_file)
+    return render_template("blog.html",
+                           data="data",
+                           title="Blog",
+                           image_file=image_file)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -161,15 +168,33 @@ def signup():
                            data="data",
                            title="Sign up")
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hek(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Account information has been updated", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for("static",
                          filename="profile_pics/" + current_user.image_file)
     return render_template("account.html",
                            title="Account",
-                           image_file=image_file)
+                           image_file=image_file,
+                           form=form)
 
 
 @app.route("/logout")
