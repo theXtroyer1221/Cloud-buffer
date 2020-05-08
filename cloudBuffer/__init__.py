20305259af4c99735aafb7013eab27dfb3bbcbdc
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-05-08 19:23:00 +0200

    getting domne, making a branch for merging with master

--- cloudBuffer/__init__.py
+++ cloudBuffer/__init__.py
@@ -29,9 +29,8 @@ login_manager.login_message_category = "info"
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config[
-    "MAIL_USERNAME"] = "jaddou2005@gmail.com"  #os.environ.get("EMAIL_USER")
-app.config["MAIL_PASSWORD"] = "huvolyeqgnbtmpvk"  #os.environ.get("EMAIL_PASS")
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
 mail = Mail(app)
 
 from cloudBuffer import routes
