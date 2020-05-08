dd43822281111c23463e277ed2c921a0e4d444d6
theXtroyer1221 <jaddou2005@gmail.com>
2020-05-08 07:57:46 +0000

    made changes to the navbar in the login page, mobile support is bleah

--- cloudBuffer/routes.py
+++ cloudBuffer/routes.py
@@ -259,7 +259,7 @@ def new_post():
 def post(post_id):
     post = Post.query.get_or_404(post_id)
 
-    return render_template("post.html", title=post.title, post=post)
+    return render_template("post.html", title=post.title, post=post, data="post_site")
 
 
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
