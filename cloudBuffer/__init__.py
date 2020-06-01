e62f55d9ac1c378371827e2594aa1e5af2e6b70c
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-06-01 20:30:07 +0200

    removed database

--- cloudBuffer/__init__.py
+++ cloudBuffer/__init__.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask import Flask
@@ -25,8 +26,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
-#whooshee = Whooshee(app)
-#app.config["WHOOSH_BASE"] = "whoosh"
+migrate = Migrate(app, db)
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
---
+++ cloudBuffer/migrate.py
@@ -0,0 +1,4 @@
+from cloudBuffer.__init__ import app, manager
+
+if __name__ == "__main__":
+    manager.run()
